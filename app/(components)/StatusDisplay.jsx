80cad83e8e104afd5b47203d1de0f716113d952f
Arbaaz <arbaaz970@gmail.com>
2024-01-13 11:16:39 +0300

    created ticketcard and all components within

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,12 @@
+const StatusDisplay = () => {
+  return (
+    <span
+      className="inline-block rounded-full px-2 py-1 text-xs 
+    font-semibold text-gray-700 bg-green-200"
+    >
+      Done.... Or am I?
+    </span>
+  );
+};
+
+export default StatusDisplay;
