003fa290f41765769f9731a4a13b6f69b9e37391
Arbaaz <arbaaz970@gmail.com>
2024-01-13 11:21:21 +0300

    created responsive grid layout

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,10 @@ import TicketCard from "./(components)/TicketCard";
 
 const Dashboard = () => {
   return (
-    <div>
-      <TicketCard />
+    <div className="p-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <TicketCard />
+      </div>
     </div>
   );
 };
